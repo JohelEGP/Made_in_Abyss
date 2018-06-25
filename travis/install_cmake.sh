c4f175873aadc057dd66e2f680306141b6a389f2
Johel Ernesto Guerrero Peña <johelegp@gmail.com>
2018-06-25 00:06:58 -0400

    Update CI CMake version

--- travis/install_cmake.sh
+++ travis/install_cmake.sh
@@ -3,7 +3,8 @@ set -ev
 
 cmake_version=3.11
 cmake_patch=3
-cmake_installer=cmake-${cmake_version}.${cmake_patch}-Linux-x86_64.sh
+cmake_pre=-rc1
+cmake_installer=cmake-${cmake_version}.${cmake_patch}${cmake_pre}-Linux-x86_64.sh
 
 cd /tmp
 wget https://cmake.org/files/v${cmake_version}/${cmake_installer}
