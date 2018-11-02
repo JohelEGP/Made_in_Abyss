d3c5aa7ef3dd471a8475b6397dcdc6546899f81c
Johel Ernesto Guerrero Peña <johelegp@gmail.com>
2018-11-02 14:37:00 -0400

    Add OS X and Clang 7 to CI

--- travis/install_clang.sh
+++ travis/install_clang.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+# Usage: ./install_clang.sh <clang_version>
+# <clang_version> is a semver of the form X.Y.Z
 set -ev
 
-clang_version=6.0.0
+clang_version=$1
 clang_archive=clang+llvm-${clang_version}-x86_64-linux-gnu-ubuntu-14.04.tar.xz
 
 cd /tmp
